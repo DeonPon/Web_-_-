960f3668dc1306bb7e1a65177c37f90c1d9ebedc
DeonPon <chesnishyidenys@ieu.edu.ua>
2024-11-25 12:56:00 +0200

    update style

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const carouselImages = document.querySelector('.carousel-images');
-const images = document.querySelectorAll('.carousel-images img');
-const prevButton = document.querySelector('.carousel-btn.prev');
-const nextButton = document.querySelector('.carousel-btn.next');
-
-let currentIndex = 0;
-
-function updateCarousel() {
-    const width = images[0].clientWidth;
-    carouselImages.style.transform = `translateX(-${currentIndex * width}px)`;
-}
 
 prevButton.addEventListener('click', () => {
     currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
@@ -22,9 +11,4 @@ nextButton.addEventListener('click', () => {
 
 window.addEventListener('resize', updateCarousel);
 
-// Работа с видео
-const video = document.getElementById('background-video');
-
-// Уменьшаем скорость воспроизведения
-video.playbackRate = 1;
 
